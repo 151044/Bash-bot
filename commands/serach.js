453ba49f8625936114373d5593c91be5d2d763ee
151044 <151044@std.stpaulscollege.edu.hk>
2020-06-14 22:34:53 +0800

    search.js

---
+++ commands/serach.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name:"search",
+    description:"Searches something on the internet.",
+    isHidden:true,
+    execute(message,args,sudo){
+        return message.channel.send(`https://duckduckgo.com/?t=ffab&q=${args[0]}&ia=web`);
+    },
+    fullMsg(){
+        const main = require("../main.js");
+        return main.embedFrom("Bash","Help",`
+        ${this.name}---${this.description}
+        
+        Syntax:
+        ~search [keyword]
+
+        Arguments:
+        keyword---The keyword to search
+
+        Optional Arguments:
+        None
+
+        Requires sudo:
+        False
+        
+        Note:
+        Not applicatable.
+
+        `);
+    }
+}
